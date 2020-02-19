c2b78d22e85f8212ff6056d0a09bea0fa1ad86b5
illia-bashchuk <ilvasilich@gmail.com>
2020-02-19 03:36:00 +0200

    add routs for views

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,88 @@
 |
 */
 
+/*
+|--------------------------------------------------------------------------
+| Українська локалізація
+|--------------------------------------------------------------------------
+|
+|
+*/
+
 Route::get('/', function () {
+    App::setLocale('uk');
     return view('index');
+});
+
+Route::get('/windows', function () {
+    App::setLocale('uk');
+    return view('windows');
+});
+
+Route::get('/doors', function () {
+    App::setLocale('uk');
+    return view('doors');
+});
+
+Route::get('/catalog', function () {
+    App::setLocale('uk');
+    return view('catalog');
+});
+
+Route::get('/profile', function () {
+    App::setLocale('uk');
+    return view('profile');
+});
+
+Route::get('/furniture', function () {
+    App::setLocale('uk');
+    return view('furniture');
+});
+
+Route::get('/sale', function () {
+    App::setLocale('uk');
+    return view('sale');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Російська локалізація
+|--------------------------------------------------------------------------
+|
+|
+*/
+
+Route::get('/ru', function () {
+    App::setLocale('ru');
+    return view('index');
+});
+
+Route::get('/ru/windows', function () {
+    App::setLocale('ru');
+    return view('windows');
+});
+
+Route::get('/ru/doors', function () {
+    App::setLocale('ru');
+    return view('doors');
+});
+
+Route::get('/ru/catalog', function () {
+    App::setLocale('ru');
+    return view('catalog');
+});
+
+Route::get('/ru/profile', function () {
+    App::setLocale('ru');
+    return view('profile');
+});
+
+Route::get('/ru/furniture', function () {
+    App::setLocale('ru');
+    return view('furniture');
+});
+
+Route::get('/ru/sale', function () {
+    App::setLocale('ru');
+    return view('sale');
 });
