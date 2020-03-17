284da1d6ab745a78a3faeb27d2c70e9e256fb337
illia-bashchuk <ilvasilich@gmail.com>
2020-03-17 05:18:48 +0200

    add uk and ru messages for site

---
+++ resources/lang/ru/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+// resources/lang/ru/messages.php
+
+return [
+    'vikna' => 'Окна',
+    'dveri' => 'Двери',
+    'katalog' => 'Каталог',
+    'profil' => 'Профиль',
+    'furnitura' => 'Фурнитура',
+    'rozprodazh' => 'Разпродажа',
+    'roztashuvania' => 'Наше местоположение',
+    'btn-forma' => 'Заказать замер',
+];
---
+++ resources/lang/uk/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+// resources/lang/uk/messages.php
+
+return [
+    'vikna' => 'Вікна',
+    'dveri' => 'Двері',
+    'katalog' => 'Каталог',
+    'profil' => 'Профіль',
+    'furnitura' => 'Фурнітура',
+    'rozprodazh' => 'Розпродаж',
+    'roztashuvania' => 'Наше розташування',
+    'btn-forma' => 'Замовити замір',
+];
