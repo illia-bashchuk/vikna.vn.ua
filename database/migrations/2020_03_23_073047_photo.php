24e79dc098d33ad8f6e22891f4f16d7fe1f5b321
illia-bashchuk <ilvasilich@gmail.com>
2020-03-24 13:56:34 +0200

    add migration for Photo model

---
+++ database/migrations/2020_03_23_073047_photo.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Photo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photo_path', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+            $table->increments('id');
+            $table->string('path', 100);
+            $table->string('url', 100);
+            $table->string('page', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('photo_path');
+    }
+}
