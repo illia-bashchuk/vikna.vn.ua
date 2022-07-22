a3ba298abf5f8005f0d45cc57d16ef50d62a2d65
Illia Bashchuk <ilvasilich@gmail.com>
2022-07-23 01:39:07 +0300

    optimazation navbar links

--- resources/views/components/language-switch.blade.php
+++ resources/views/components/language-switch.blade.php
@@ -1,17 +1,7 @@
 <div class="language-switch">
     <nav class="nav justify-content-center">
-        <a class="nav-link {{ app()->getLocale() === 'uk' ? 'my-active' : ''}}" href="
-        @if (app()->getLocale() === 'uk')
-            {{url()->current()}}
-        @elseif (app()->getLocale() === 'ru')
-            {{url('uk/'.request()->segment(2))}}
-        @endif">укр</a>
-        <a class="nav-link {{ app()->getLocale() === 'ru' ? 'my-active' : ''}}" href="
-        @if (app()->getLocale() === 'uk' )
-            {{url('ru/'.request()->segment(2))}}
-        @elseif (app()->getLocale() === 'ru')
-            {{url()->current()}}
-        @endif">рус</a>
+    <a class="nav-link {{ app()->getLocale() === 'uk' ? 'my-active' : ''}}" href="{{$ukr_url}}">укр</a>
+    <a class="nav-link {{ app()->getLocale() === 'ru' ? 'my-active' : ''}}" href="{{$rus_url}}">рус</a>
     </nav>
 </div>
 
